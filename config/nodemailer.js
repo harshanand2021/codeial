ee7395892ed2bf60ba7a44a6599d5ef9b5d3943e
harshanand2021 <harsh250600@gmail.com>
2023-09-12 18:54:15 +0530

    HTML templates email

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,35 +1,37 @@
 const nodemailer = require("nodemailer");
-const ejs = require('ejs');
-const path = require('path');
-
+const ejs = require("ejs");
+const path = require("path");
 
 let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 507,
-    secure: false,
-    auth: {
-        user: 'harsh250600',
-        pass: 'Harsh@2023'
-    }
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 507,
+  secure: false,
+  auth: {
+    user: "harsh250600",
+    pass: "Harsh@2023",
+  },
 });
 
 let renderTemplate = (data, relativePath) => {
-    let mailHTML;
-    ejs.renderFile(
-        path.join(__dirname, '../views/mailers', relativePath),
-        data,
-        function(err, template){
-            if(err){console.log("Error in rendering template");return;}
+  let mailHTML;
+  ejs.renderFile(
+    path.join(__dirname, "../views/mailers", relativePath),
+    data,
+    function (err, template) {
+      if (err) {
+        console.log("Error in rendering template", err);
+        return;
+      }
 
-            mailHTML = template;
-        }
-    )
+      mailHTML = template;
+    }
+  );
 
-    return mailHTML;
-}
+  return mailHTML;
+};
 
 module.exports = {
-    transporter: transporter,
-    renderTemplate: renderTemplate
-}
+  transporter: transporter,
+  renderTemplate: renderTemplate,
+};
